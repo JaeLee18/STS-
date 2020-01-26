ef605ab7bbd7c52deb2cb0f7183d104189bd7658
JaeJoong Lee <approach0218@gmail.com>
2020-01-26 16:39:46 +0900

    Added a controller

--- api/src/main/java/api/topic/TopicController.java
+++ api/src/main/java/api/topic/TopicController.java
@@ -1,22 +1,22 @@
 package api.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SuppressWarnings("unused")
 @RestController
 public class TopicController {
 
+		@Autowired
+		private TopicService topicservice;
+		
 		@RequestMapping("/topics")
 		// Returning Topic objects in List Type
 		public List<Topic> getAllTopics() {
 			// Here to connect 
-			return Arrays.asList(
-					new Topic("spring", "Spring.framework", "Spring Framework description"),
-					new Topic("java", "java framework", "java Framework description"),
-					new Topic("python", "flask", "python description")
-					);
+			return topicservice.getAllTopics();
 		}
 }
