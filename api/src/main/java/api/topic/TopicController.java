773c4c61639a8333f68c2041dfc184547b36c3e4
JaeJoong Lee <approach0218@gmail.com>
2020-01-26 18:00:20 +0900

    Create, Delete, Upadte, Fetech API

--- api/src/main/java/api/topic/TopicController.java
+++ api/src/main/java/api/topic/TopicController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SuppressWarnings("unused")
@@ -14,6 +16,7 @@ public class TopicController {
 		@Autowired
 		private TopicService topicservice;
 		
+		// Get
 		@RequestMapping("/topics")
 		// Returning Topic objects in List Type
 		public List<Topic> getAllTopics() {
@@ -21,8 +24,28 @@ public class TopicController {
 			return topicservice.getAllTopics();
 		}
 		
+		// Get
 		@RequestMapping("/topics/{id}")
 		public Topic getTopic(@PathVariable String id) {
 			return topicservice.getTopic(id);
+		}
+		
+		
+		@RequestMapping(method=RequestMethod.POST, value="/topics")
+		public void addTopic(@RequestBody Topic topic) {
+			topicservice.addTopic(topic);
+			
+		}
+		
+		@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+		public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+			topicservice.updateTopic(id, topic);
+			
+		}
+		
+		@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+		public void deleteTopic(@PathVariable String id) {
+			topicservice.deleteTopic(id);
+			
 		}
 }
